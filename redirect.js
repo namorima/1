ccad2d2987b23e172b8822b21a484b1a2b526920
namorima <84259399+namorima@users.noreply.github.com>
2025-07-06 16:47:44 +0800

    Update multi link to redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,4 @@
-window.location.href = "https://drive.google.com/file/d/1-FegByPkqMiBut2d3tnK_stVNlfpIFrj/view?usp=drive_link";
+const redirectLinks = {
+    'drive': 'https://drive.google.com/file/d/1-FegByPkqMiBut2d3tnK_stVNlfpIFrj/view?usp=drive_link',
+    'file': 'https://www.microsoft.com/en-my/microsoft-365/onedrive/online-cloud-storage'
+};
