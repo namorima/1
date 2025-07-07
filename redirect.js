9ea66f8c8a2878920a86f38e891b247c523b1ebe
namorima <84259399+namorima@users.noreply.github.com>
2025-07-07 17:02:37 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,5 @@
 const redirectLinks = {
     'drive': 'https://drive.google.com/file/d/1-FegByPkqMiBut2d3tnK_stVNlfpIFrj/view?usp=drive_link',
-    'file': 'https://www.microsoft.com/en-my/microsoft-365/onedrive/online-cloud-storage'
+    'file': 'https://www.microsoft.com/en-my/microsoft-365/onedrive/online-cloud-storage',
+    'bat': 'https://johnadik.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiVCs0ekJsUFBkVWFRUFMwczFyblJxNzdnYmIrdzhjanhVT2hLT0NnUzN4OFF0SlBEekNDVnFDK3BLT3pLZkxJM1hBNEFNYkdQMlUxbWJVVGs2NytMbC9MTi9RRHNWWjBsUlJMTnQwSkJHWFY3VnpNMEZTWVlTdk1lSnJweVN1cG8iLCJoIjoiR2l2ZSBNZSBUaGUgTmFtZSIsImkiOiI3UzFMU3dXMTg3YmM4MHdaIn0='
 };
