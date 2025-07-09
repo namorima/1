c2097d8017526ed169c97c418f82d102ec51f446
namorima <84259399+namorima@users.noreply.github.com>
2025-07-09 12:44:02 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,6 @@
 const redirectLinks = {
     'drive': 'https://drive.google.com/file/d/1-FegByPkqMiBut2d3tnK_stVNlfpIFrj/view?usp=drive_link',
     'file': 'https://www.microsoft.com/en-my/microsoft-365/onedrive/online-cloud-storage',
-    'bat': 'https://johnadik.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiVCs0ekJsUFBkVWFRUFMwczFyblJxNzdnYmIrdzhjanhVT2hLT0NnUzN4OFF0SlBEekNDVnFDK3BLT3pLZkxJM1hBNEFNYkdQMlUxbWJVVGs2NytMbC9MTi9RRHNWWjBsUlJMTnQwSkJHWFY3VnpNMEZTWVlTdk1lSnJweVN1cG8iLCJoIjoiR2l2ZSBNZSBUaGUgTmFtZSIsImkiOiI3UzFMU3dXMTg3YmM4MHdaIn0='
+    'bat': 'https://johnadik.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiVCs0ekJsUFBkVWFRUFMwczFyblJxNzdnYmIrdzhjanhVT2hLT0NnUzN4OFF0SlBEekNDVnFDK3BLT3pLZkxJM1hBNEFNYkdQMlUxbWJVVGs2NytMbC9MTi9RRHNWWjBsUlJMTnQwSkJHWFY3VnpNMEZTWVlTdk1lSnJweVN1cG8iLCJoIjoiR2l2ZSBNZSBUaGUgTmFtZSIsImkiOiI3UzFMU3dXMTg3YmM4MHdaIn0=',
+    'hadir': 'https://script.google.com/a/~/macros/s/AKfycbwiQPZImyeNyr-dCAzn68aHWa78cLQakt-OWapg0cWwBZY0ZLowQBjFIRAMJj4I6AFG1A/exec'
 };
