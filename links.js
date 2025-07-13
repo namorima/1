5017ab311cc57f813769aa481e40e3b4cf9f0be1
namorima <84259399+namorima@users.noreply.github.com>
2025-07-13 16:09:20 +0800

    Create links.js

---
+++ links.js
@@ -0,0 +1,18 @@
+// links.js
+const links = [
+  {
+    title: "Google",
+    url: "https://www.google.com",
+    desc: "Enjin carian paling popular."
+  },
+  {
+    title: "YouTube",
+    url: "https://www.youtube.com",
+    desc: "Tonton video percuma."
+  },
+  {
+    title: "Shopee",
+    url: "https://shopee.com.my",
+    desc: "Laman e-commerce popular Malaysia."
+  }
+];
